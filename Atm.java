bf3270ac9c66e4d123396a74db4979d6630ec7aa
paowick <korn2k9@gmail.com>
2022-12-15 02:02:07 +0700

    Update Atm.java

--- Atm.java
+++ Atm.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 public class Atm {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         ArrayList<Account> accounts = new ArrayList<Account>();
         accounts.add(new Account("keter", "01", "1234", 1000));
         accounts.add(new Account("binah", "02", "1234", 1230));
@@ -12,31 +13,31 @@ public class Atm {
         accounts.add(new Account("gevurah", "05", "1234", 880));
         accounts.add(new Account("tiferet", "06", "1234", 1110));
 
+        while (true) {
         System.out.print("Step 1. Enter amount of all account = ");
-        int accAmount = Integer.parseInt(new Scanner(System.in).nextLine());
+        int accAmount = Integer.parseInt(scanner.nextLine());
         System.out.println("Step 2. Enter Detail of each account.");
         for (int i = 0; i < accAmount; i++) {
             System.out.print("No.");
             System.out.println(i + 1);
             System.out.print("Account ID = ");
-            String createID = new Scanner(System.in).nextLine().toString();
+            String createID = scanner.nextLine().toString();
             System.out.print("Account Name = ");
-            String createName = new Scanner(System.in).nextLine().toString();
+            String createName = scanner.nextLine().toString();
             System.out.print("Passowrd =");
-            String createPassWord = new Scanner(System.in).nextLine().toString();
+            String createPassWord = scanner.nextLine().toString();
             System.out.print("Balance =");
-            int createBalance = Integer.parseInt(new Scanner(System.in).nextLine());
+            int createBalance = Integer.parseInt(scanner.nextLine());
             accounts.add(new Account(createName, createID, createPassWord, createBalance));
         }
 
-        while (true) {
             try {
                 System.out.println("=====================================================");
                 System.out.println("ATM ComputerThanyaburiBank");
                 System.out.print("Account ID :");
-                String ID = new Scanner(System.in).nextLine().toString();
+                String ID = scanner.nextLine().toString();
                 System.out.print("Account Password : ");
-                String PASS = new Scanner(System.in).nextLine().toString();
+                String PASS = scanner.nextLine().toString();
 
                 for (int i = 0; i < accounts.size(); i++) {
                     if (accounts.get(i).getId().equals(ID) && accounts.get(i).getPassWord().equals(PASS)) {
@@ -51,7 +52,7 @@ public class Atm {
                                 System.out.println("3.Exit");
                                 System.out.print("choose : ");
 
-                                String choose = new Scanner(System.in).nextLine().toString();
+                                String choose = scanner.nextLine().toString();
 
                                 if (choose.equals("1")) {
                                     System.out.println("========================================= ");
@@ -62,7 +63,7 @@ public class Atm {
                                     System.out.println("========================================= ");
                                 } else if (choose.equals("2")) {
                                     System.out.print("Enter amount : ");
-                                    int moneyWithDraw = Integer.parseInt(new Scanner(System.in).nextLine());
+                                    int moneyWithDraw = Integer.parseInt(scanner.nextLine());
 
                                     if (moneyWithDraw > accounts.get(i).getMoney()) {
                                         System.out.println("========================================= ");
@@ -81,19 +82,27 @@ public class Atm {
                                 }
 
                             } catch (Exception e) {
-                                System.out.println("something worng");
+                                System.out.println("something worng2");
+                                break;
                             }
 
                         }
 
+                    }else{
+                    
                     }
                 }
 
             } catch (Exception e) {
-                System.out.println("something worng");
+                System.out.println("something worng1");
             }
 
+            
         }
+        
+    
+        
     }
+
 
 }
